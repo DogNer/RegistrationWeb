bba5af850fb4d125abb8f852b2e1fc050a88de42
Dana <danamushchenko@gmail.com>
2024-07-02 17:24:29 +0300

    add design to registration window

--- code/script.js
+++ code/script.js
@@ -1,4 +1,38 @@
+window.onload = function() {
+    var JSONData = JSON.parse(localStorage.getItem('JSONData')) || [{
+        "name": "",
+        "email": "",
+        "password": ""
+    }];
 
-document.getElementById('btnSubmit').addEventListener('click', function() {
-    alert('Button clicked');
-});
+    console.log(JSONData);
+
+    document.getElementById('btnSubmit').addEventListener('click', function(event) {
+        try {
+            let name = document.getElementById('name').value;
+            let email = document.getElementById('email').value;
+            let pass = document.getElementById('password').value;
+            let pass2 = document.getElementById('passwordConfirm').value;
+
+            if (name == "" || email == "" || pass == "" || pass2 == "") {
+                throw new Error("Please fill out all fields");
+            }
+
+            if (pass != pass2) {
+                throw new Error("Passwords do not match");
+            }
+
+            JSONData.push({
+                "name": name,
+                "email": email,
+                "password":  pass
+            });
+
+            localStorage.setItem('JSONData', JSON.stringify(JSONData));
+        }
+        catch (error) {
+            console.log(error.message);
+            alert(error.message);
+        }
+    });
+};
